5117e5c0fe547f428cd4482a8dd83964bbf17cf2
Abhinav Shrestha and Cameron Cundiff <pair+ashrestha+ccundiff@pivotallabs.com>
2014-01-24 16:15:36 -0500

    Remove sauce driver.

--- Gemfile
+++ Gemfile
@@ -2,9 +2,6 @@ source 'https://rubygems.org'
 
 gemspec
 
-gem "sauce"
-gem "sauce-connect"
-
 group :development, :test do
   gem "tddium"
   gem "sinatra"
--- lib/capybara/accessible.rb
+++ lib/capybara/accessible.rb
@@ -1,6 +1,5 @@
 require 'capybara'
 require 'capybara/webkit'
-require 'sauce/capybara'
 require 'capybara/accessible/auditor'
 require 'capybara/accessible/element'
 require "capybara/accessible/version"
@@ -13,7 +12,6 @@ end
 
 require "capybara/accessible/driver"
 require "capybara/accessible/webkit_driver"
-require "capybara/accessible/sauce_driver"
 
 Capybara.register_driver :accessible do |app|
   Capybara::Accessible::Driver.new(app)
@@ -21,8 +19,4 @@ end
 
 Capybara.register_driver :accessible_webkit do |app|
   Capybara::Accessible::WebkitDriver.new(app)
-end
-
-Capybara.register_driver :accessible_sauce do |app|
-  Capybara::Accessible::SauceDriver.new(app)
 end
