d434708ad0435bfdd56ec149a214077c82d663b0
Michael Lavrisha <vrish88@gmail.com>
2014-02-06 11:57:54 -0700

    move definitions to more appropriate location

--- lib/capybara/accessible.rb
+++ lib/capybara/accessible.rb
@@ -7,6 +7,65 @@ require "capybara/accessible/railtie" if defined?(Rails)
 
 module Capybara
   module Accessible
+    class SeleniumDriverAdapter
+      def modal_dialog_present?(driver)
+        begin
+          driver.browser.switch_to.alert
+          true
+        rescue ::Selenium::WebDriver::Error::NoAlertOpenError, ::NoMethodError
+          false
+        end
+      end
+
+      def failures_script
+        "return axs.Audit.auditResults(results).getErrors();"
+      end
+
+      def create_report_script
+        "return axs.Audit.createReport(results);"
+      end
+
+      def run_javascript(driver, script)
+        driver.execute_script(script)
+      end
+    end
+
+    class WebkitDriverAdapter
+      def modal_dialog_present?(driver)
+        driver.alert_messages.any?
+      end
+
+      def failures_script
+        "axs.Audit.auditResults(results).getErrors();"
+      end
+
+      def create_report_script
+        "axs.Audit.createReport(results);"
+      end
+
+      def run_javascript(driver, script)
+        driver.evaluate_script(script)
+      end
+    end
+
+    class << self
+      def skip_audit
+        @disabled = true
+        yield
+      ensure
+        @disabled = false
+      end
+
+      def driver_adapter
+        @driver_adapter
+      end
+
+      def setup(driver, adaptor)
+        @driver_adapter = adaptor
+        driver.extend(Capybara::Accessible::DriverExtensions)
+        driver
+      end
+    end
   end
 end
 
--- lib/capybara/accessible/auditor.rb
+++ lib/capybara/accessible/auditor.rb
@@ -1,66 +1,6 @@
 module Capybara::Accessible
   class InaccessibleError < Capybara::CapybaraError; end
 
-  class SeleniumDriverAdapter
-    def modal_dialog_present?(driver)
-      begin
-        driver.browser.switch_to.alert
-        true
-      rescue ::Selenium::WebDriver::Error::NoAlertOpenError, ::NoMethodError
-        false
-      end
-    end
-
-    def failures_script
-      "return axs.Audit.auditResults(results).getErrors();"
-    end
-
-    def create_report_script
-      "return axs.Audit.createReport(results);"
-    end
-
-    def run_javascript(driver, script)
-      driver.execute_script(script)
-    end
-  end
-
-  class WebkitDriverAdapter
-    def modal_dialog_present?(driver)
-      driver.alert_messages.any?
-    end
-
-    def failures_script
-      "axs.Audit.auditResults(results).getErrors();"
-    end
-
-    def create_report_script
-      "axs.Audit.createReport(results);"
-    end
-
-    def run_javascript(driver, script)
-      driver.evaluate_script(script)
-    end
-  end
-
-  class << self
-    def skip_audit
-      @disabled = true
-      yield
-    ensure
-      @disabled = false
-    end
-
-    def driver_adapter
-      @driver_adapter
-    end
-
-    def setup(driver, adaptor)
-      @driver_adapter = adaptor
-      driver.extend(Capybara::Accessible::DriverExtensions)
-      driver
-    end
-  end
-
   class Auditor
     class Node < self
       def initialize(session)
